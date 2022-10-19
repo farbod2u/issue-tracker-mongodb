253eb6d805db9c1257e2426f58da1ff7b357d153
saeed <saeedsaf@gmail.com>
2022-10-19 18:40:28 +0330

    add Issue class

---
+++ front-end/src/app/model/issue.ts
@@ -0,0 +1,14 @@
+export class Issue {
+    // @ts-ignore
+    _id: string;
+    // @ts-ignore
+    title: string;
+    // @ts-ignore
+    responsible: string;
+    // @ts-ignore
+    description: string;
+    // @ts-ignore
+    severity: string;
+    // @ts-ignore
+    status: string;
+}
