af45b4b0e9c01f13febff44200b023ece7825836
saeed <saeedsaf@gmail.com>
2022-10-25 14:00:59 +0330

    issue list form

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,31 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { HttpClientModule } from "@angular/common/http";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatTableModule} from "@angular/material/table";
+import {MatDividerModule} from "@angular/material/divider";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IssueListComponent } from './components/issue-list/issue-list.component';
-import { IssueCreateComponent } from './components/issue-create/issue-create.component';
-import { IssueEditComponent } from './components/issue-edit/issue-edit.component';
+import {HttpClientModule} from "@angular/common/http";
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {IssueListComponent} from './components/issue-list/issue-list.component';
+import {IssueCreateComponent} from './components/issue-create/issue-create.component';
+import {IssueEditComponent} from './components/issue-edit/issue-edit.component';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IssueListComponent,
-    IssueCreateComponent,
-    IssueEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IssueListComponent,
+        IssueCreateComponent,
+        IssueEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        HttpClientModule,
+        MatDividerModule, MatOptionModule, MatSelectModule, MatIconModule, MatButtonModule,
+        MatSnackBarModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatCardModule, MatTableModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- front-end/src/app/components/issue-list/issue-list.component.ts
+++ front-end/src/app/components/issue-list/issue-list.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {MatTableDataSource} from "@angular/material/table";
+import {IssueService} from "../../services/issue.service";
+import {Issue} from "../../model/issue";
 
 @Component({
-  selector: 'app-issue-list',
-  templateUrl: './issue-list.component.html',
-  styleUrls: ['./issue-list.component.css']
+    selector: 'app-issue-list',
+    templateUrl: './issue-list.component.html',
+    styleUrls: ['./issue-list.component.css']
 })
 export class IssueListComponent implements OnInit {
+    // @ts-ignore
+    issues: Issue[];
+    displayedColumns = ['title', 'responsible', 'severity', 'status', 'actions'];
 
-  constructor() { }
+    constructor(private issueService: IssueService, private router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getAll();
+    }
+
+    getAll() {
+        // @ts-ignore
+        this.issueService.getAll().subscribe(
+            {
+                next: (v: Issue[]) => {
+                    console.log(v);
+                    this.issues = v;
+                },
+                error: e => console.log("error on getAll issues", e)
+            });
+    }
+
+    edit(id: string) {
+        this.router.navigate([`/issue-edit/ ${id}`]);
+    }
+
+    delete(id: string) {
+        this.issueService.delete(id).subscribe({
+            next: () => this.getAll(),
+            error: e => console.log("error on delete issue # " + id, e)
+        });
+    }
+
+    create() {
+        this.router.navigate([`/issue-create/`]);
+    }
 
 }
